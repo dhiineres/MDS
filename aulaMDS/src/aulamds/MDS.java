c53918580600f7227e1fe2f4afc211281f53f6fd
alunoinf <alunoinf@pc-649301.(none)>
2015-10-05 21:08:46 -0300

    parte 1

---
+++ aulaMDS/src/aulamds/MDS.java
@@ -0,0 +1,18 @@
+package aulamds;
+
+public class MDS {
+
+    public static void main(String[] args) {
+        int x = 5;
+        char letra = 'm';
+        String palavra = "palavra";
+        boolean assercao = true;
+        assercao = false;
+        
+        if (assercao){
+            System.out.println(x + "é par");
+        }else{
+            System.out.println("x");
+        }
+    }
+}
